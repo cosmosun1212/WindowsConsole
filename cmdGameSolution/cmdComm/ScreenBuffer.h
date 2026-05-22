c557a5a7db8dedcec1a3a6a6fa39a6ac6d19f736
Kim DaeJin <78810280+cosmosun1212@users.noreply.github.com>
2026-05-22 15:57:06 +0900

    Refactor screen buffer header for default dimensions

--- cmdGameSolution/cmdComm/ScreenBuffer.h
+++ cmdGameSolution/cmdComm/ScreenBuffer.h
@@ -1,13 +1,17 @@
 #pragma once
+
 #include <tchar.h>
 
-// 화면 크기 상수 정의
-const int SCREEN_WIDTH = 80;
-const int SCREEN_HEIGHT = 25;
+const int DEFAULT_SCREEN_WIDTH = 80;
+const int DEFAULT_SCREEN_HEIGHT = 25;
 
-void ScreenInit();
+void ScreenInit(int width = DEFAULT_SCREEN_WIDTH, int height = DEFAULT_SCREEN_HEIGHT);
+
 void ScreenFlipping();
 void ScreenClear();
 void ScreenRelease();
 void ScreenPrint(int x, int y, const wchar_t* string);
 void SetColor(unsigned short color);
+
+int GetScreenWidth();
+int GetScreenHeight();
