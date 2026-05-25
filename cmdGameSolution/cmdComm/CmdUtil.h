d455d704e85902ab02d5035ea43d4070fe2913be
Kim DaeJin <78810280+cosmosun1212@users.noreply.github.com>
2026-05-26 00:41:31 +0900

    Delete cmdGameSolution directory

--- cmdGameSolution/cmdComm/CmdUtil.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-void DrawRect(int x, int y, int w, int h, unsigned short color);
--- cmdGameSolution/cmdProject001/GameUI.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "TimeManager.h"
-#include "InputManager.h"
-
-void InitScreen(int cols, int rows, int color);
-
-void FPS_Gizmo(int x, int y, TIMEMANAGER* pTime, INPUTVALUE* pInput);
